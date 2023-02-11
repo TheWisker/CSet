712277d0b25a7f0bd7ba47e9d75c1d7245cd8fa6
TheWisker <stefanobia227@gmail.com>
2023-02-11 11:36:59 +0100

    Scripts updated

--- scripts/cross.sh
+++ scripts/cross.sh
@@ -1,11 +1,27 @@
-meson setup ./bin
-meson setup ./bin/win --cross-file assets/crosswin.txt
+source_path=$(cd "$(dirname ".")"; pwd)/$(basename ".")
+rm -fr ./bin
+rm -fr ./doc
+rm -fr ./dist
+mkdir -v ./bin
+mkdir -v ./bin/win
+meson setup ./bin --prefix=$source_path/
+meson setup ./bin/win --cross-file assets/crosswin.txt --prefix=$source_path/
+cd ./bin
+meson configure --buildtype=release --optimization=3
+meson compile
+cd ../
 git add .
 git commit -m "Cross compilation executing"
 cd ./bin
-meson dist
+meson dist --no-tests
 cd ./win
-meson dist --formats zip
+meson configure --buildtype=release --optimization=3
+meson compile
+cd ../../
+git add .
+git commit -m "Cross compilation executing"
+cd ./bin/win
+meson dist --formats zip --no-tests
 mkdir -v ../../dist/
 cp -r ./meson-dist/* ../../dist/
 cd ..
@@ -13,3 +29,4 @@ cp -r ./meson-dist/* ../dist/
 cd ..
 git add .
 git commit -m "Cross compilation executed"
+unset source_path
